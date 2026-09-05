da3044c446bf39bc81d2341c4316e4d12c435a95
huhan <hh@huhandeMacBook-Pro.local>
2026-09-05 19:37:04 +0800

    Update Scopy cask to v0.80.4

--- Casks/scopy.rb
+++ Casks/scopy.rb
@@ -1,6 +1,6 @@
 cask "scopy" do
-  version "0.80.3"
-  sha256 "1e4eb7329dcc7efbbf01f7756a18d52877abe685837c0ed99ee89eec3aacd70b"
+  version "0.80.4"
+  sha256 "84a3d70ebd4fe7941b2f998ccedfc25396f34003ca79f248d14b34eaf591bf85"
 
   url "https://github.com/Suehn/Scopy/releases/download/v#{version}/Scopy-#{version}.dmg"
   name "Scopy"
